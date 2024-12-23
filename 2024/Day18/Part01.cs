a2fe061e3ba90d094ec3d3dd5bd79d9568dbe11c
marc.scheffner@br.de <marc.scheffner@br.de>
2024-12-23 17:46:38 +0100

    Day18 start

---
+++ 2024/Day18/Part01.cs
@@ -0,0 +1,20 @@
+namespace Day18;
+public class Part01
+{
+    private char[,] MemoryField;
+    private int MaxAllowedBytes = 1024;
+    public long Result(ReadOnlySpan<string> input)
+    {
+        InputParser(input);
+    }
+
+    private void InputParser(ReadOnlySpan<string> input)
+    {
+        for (int i = 0; i < MaxAllowedBytes; i++)
+        {
+
+        }
+
+
+    }
+}
---
+++ 2024/Day18/Part02.cs
@@ -0,0 +1,10 @@
+
+namespace Day18;
+
+public class Part02
+{
+    public long Result(ReadOnlySpan<string> input)
+    {
+        return 0;
+    }
+}
